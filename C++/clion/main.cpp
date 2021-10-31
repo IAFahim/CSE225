0c06f0cf72875e71ffb3c1b182878a50231c2f00
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2021-10-31 12:20:53 +0600

    Lab 3

--- C++/clion/main.cpp
+++ C++/clion/main.cpp
@@ -6,24 +6,7 @@ int main() {
     int n;
     cin>>n;
     for (int i = 0; i < n; ++i) {
-        string s;
-        if(s.length()>10)
-        {
-            for(int i=0;i<s.length();i++)
-            {
-                char b=s[i];
-                char e=s[s.length()-1];
-                int a=s.length()-2;
-            }
 
-            t=b+to_string(a)+e;
-            cout<< t;
-
-        }
-        else
-        {
-            cout<< s;
-        }
     }
     return 0;
 }
