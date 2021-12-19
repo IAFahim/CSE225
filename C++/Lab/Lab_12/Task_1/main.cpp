c02edae81696f9c3f6fcb5b8f2b9b9838841aec0
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2021-12-19 08:27:37 +0600

    Lab 12

--- C++/Lab/Lab_12/Task_1/main.cpp
+++ C++/Lab/Lab_12/Task_1/main.cpp
@@ -60,33 +60,6 @@ void Task_1(T type) {
     treeNode.MakeEmpty();
 }
 
-template<class T>
-TreeNode<T> *MinSpan(T *arr, int start, int end) {
-    if (start > end)return nullptr;
-    int mid = (end + start) / 2;
-    auto *treeNode = new TreeNode<T>;
-    treeNode->info = arr[mid];
-    treeNode->left = MinSpan(arr, start, mid - 1);
-    treeNode->right = MinSpan(arr, mid + 1, end);
-    return treeNode;
-}
-
-void preOrder(TreeNode<int> *treeNode) {
-    if (treeNode == nullptr)return;
-    cout << treeNode->info << " ";
-    preOrder(treeNode->left);
-    preOrder(treeNode->right);
-}
-
-template<class T>
-void Task_2(T type) {
-    T arr[] = {11, 9, 4, 2, 7, 3, 17, 0, 5, 1};
-    sort(arr, arr + (sizeof(arr) / sizeof(arr[0])));
-    TreeNode<T> *treeNode = MinSpan(arr, 0, (sizeof(arr) / sizeof(arr[0])) - 1);
-    preOrder(treeNode);
-}
-
 int main() {
     Task_1(INT32_C(0));
-    Task_2(INT32_C(0));
 }
